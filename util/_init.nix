a30593e288fa8183e241c1a5d0ff4999d93e5778
Sam Craig <contact@samcraig.io>
2021-09-09 08:40:07 -0400

    build: add support for local derivations via local flag in util.init

--- util/_init.nix
+++ util/_init.nix
@@ -1,4 +1,8 @@
 { constants ? import ./constants.nix, sources ? import ../nix { } }:
+{ tool, version, local ? false }:
 
-let inherit (sources) nixpkgs;
-in { tool, version }: (import (constants.root-dir + "/pkgs/${tool}/${version}.nix"))
+let
+  inherit (sources) nixpkgs;
+  path = constants.root-dir + "/pkgs/${tool}/${version}.nix";
+in if local then nixpkgs.callPackage path { } else import path
+
